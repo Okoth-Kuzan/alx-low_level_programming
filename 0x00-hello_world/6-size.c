a11a3fb14311bedc32443d1aa8e788a47719a0d8
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-08-10 13:22:10 -0700

    confusing

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -  a C program that prints the size of various types on the computer it is compiled and run on
+ * Return 0
+ */
+int main (void)
+{
+	char t;
+	int e;
+	long int r;
+	long long int a;
+	float n;
+
+	printf(

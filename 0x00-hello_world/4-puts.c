61c7d32cfe88acb95d054a1222cb43d1a3e75c9d
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-08-10 12:30:37 -0700

    a Challenge

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  * main - Program to print a line using puts function and not printf function
  * Return: 0
  */
-int main() {
+int main(void) {
     puts("\"Programming is like building a multilingual puzzle,");
     return(0);
 }

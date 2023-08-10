4d16ec549ea5a8a818238bcfae842f49df7de6bb
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-08-10 09:10:31 -0700

    no c without compiler

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-
+/**
+ * main - Program to print a line using puts function and not printf function
+ * Return: 0
+ */
 int main() {
     puts("Programming is like building a multilingual puzzle,");
     return 0;

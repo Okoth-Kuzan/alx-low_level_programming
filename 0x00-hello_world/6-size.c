1710c5cca5a7a409fdeaa8f81a04fd9e306adcbd
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-08-10 13:58:03 -0700

    Betty said to many lines

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 /**
- * main -  a C program that prints the size of various types on the computer it is compiled and run on
- * Return 0
+ * main -  A C program that prints the size of various types on the computer it is compiled and run on
+ * Return 0 (Succesfull)
  */
 int main (void)
 {
-	char t;
-	int e;
-	long int r;
-	long long int a;
-	float n;
-
 	printf("Size of a char: %lu byte(s)" sizeof(char));
 	printf("Size of an int: %lu byte(s)" sizeof(int));
 	printf("Size of a long int: %lu byte(s)" sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)" sizeof(long long int));
-printf("Size of a float: %lu byte(s)" sizeof(float));
+	printf("Size of a float: %lu byte(s)" sizeof(float));
+	return (0);
 }

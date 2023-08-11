fe082edf67c1fc0183663af426fa70bf384c23d7
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-08-11 16:25:47 -0700

    ISO C forbids an empty translation unit [-Werror=pedantic]

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i <= 7; i++)
+	{
+		for (j = i + 1; j <= 8; j++)
+		{
+			for (k = j + 1; k <= 9; k++)
+			{
+				if (k > j && j > i)
+				{
+					putchar(i);
+					outchar(j);
+					putchar(k);
+					if (i != 7 || j!= 8)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

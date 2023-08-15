5850463afdcdd5db8b374c1d4a5f48e4d148abc0
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-08-15 14:31:37 -0700

    T for tough

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,9 @@
  */
 void print_alphabet_x10(void)
 {
-	int i;
 	char letter;
-	
-	for (i = 0; i < 10; i++)
-	{
-		letter = 'a'
-			while (letter <= 'z')
-			{
-				_putchar(letter);
-				letter++;
-			}
-	}
 
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 	_putchar('\n');
-
 }

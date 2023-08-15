2bf89de9b7676b79ea96150f08b98cfa30c5a057
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-08-15 15:50:36 -0700

    description for parameter

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,8 +2,8 @@
 
 /**
  * _islower -  checks for lowercase character
- * c - character to be checked
- * Return: 1 (Success)
+ * @c: character to be checked
+ * Return: 1 if @c is a lowercase letter, 0 otherwise.
  */
 int _islower(int c)
 {

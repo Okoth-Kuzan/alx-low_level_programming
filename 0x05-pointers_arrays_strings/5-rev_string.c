36f0a2705156447a0fbc68c747385c2268b1162d
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-08-21 13:44:47 -0700

    but i like for

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Input string
+ * Return: String in reverse
+ */
+
+void rev_string(char *s)
+{
+	char temp;
+	int length = 0;
+
+	while (s[length] != '\0')
+		length++;
+
+	for (int i = 0; i < length / 2; i++)
+	{
+		temp = s[i];
+		s[i] = s[length - i - 1];
+		s[length - i - 1] = temp;
+	}
+}

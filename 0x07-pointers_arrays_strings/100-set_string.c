8e286e878a8bef7387deb50121bcb9cc1157e503
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-09-11 11:11:45 -0700

    all at once

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - sets value of a pointer to a char
+ *
+ * Return: 0 (success)
+ */
+void set_string(char **s, char *to)
+{
+	*ptr = &value;
+}
+int main(void)
+{
+	char character = 'T';
+	char *charPointer = NULL;
+
+	printf("before: charPointer = %p\n", (void *)charPointer);
+
+	set_char(&charPointer, character);
+
+	printf("After: charPointer = %p, *charPointer = %c\n",(charPointer, *charPointer);
+
+			return (0);
+			}
+			

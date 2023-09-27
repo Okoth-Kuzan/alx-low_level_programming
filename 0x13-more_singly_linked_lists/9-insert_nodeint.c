d220d91cabffdb21f3faffc9db168186683af5d6
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-09-27 12:43:13 -0700

    correctons

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,36 +9,35 @@
  *
  * Return: The address of the new node, or NULL if it fails.
  */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current = *head;
-	unsigned int i;
+	listint_t *node, *new_node = malloc(sizeof(listint_t));
+	unsigned int i = 0;
 
-	if (idx == 0)
+	if (!head || !new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
+	if (!idx)
 	{
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
-			return (NULL);
-
-		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	for (i = 0; current != NULL && i < idx - 1; i++)
+	node = *head;
+	while (node)
 	{
-		current = current->next;
+		if (i == idx - 1)
+		{
+			new_node->next = node->next;
+			node->next = new_node;
+			return (new_node);
+		}
+		i++;
+		node = node->next;
 	}
-	if (current == NULL || (i == idx - 1 && current->next == NULL))
-		return (NULL);
-
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->next = current->next;
-	current->next = new_node;
-
-	return (new_node);
+	free(new_node);
+	return (NULL);
 }
-

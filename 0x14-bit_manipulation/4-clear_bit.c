829a9dc47159395ca246e64cdc353a2889728b36
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-10-26 17:45:01 -0700

    e

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - Clears the bit at the specified index.
+ * @n: Pointer to the unsigned long integer.
+ * @index: The index of the bit to clear (starting from 0).
+ *
+ * Return: 1 if successful, or -1 on error.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(*n) * 8)
+		return (-1);
+
+	*n ^= (1UL << index);
+
+	return (1);
+}

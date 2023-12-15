7505f8160d5e83898a4fb53c31acaefad601979d
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-12-15 13:44:07 -0800

    0

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @t: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *t)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (t[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (t[i] == '-')
+			++d;
+
+		if (t[i] >= '0' && t[i] <= '9')
+		{
+			digit = t[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (t[i + 1] < '0' || t[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
